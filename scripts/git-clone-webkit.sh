2c0d207cec590008943799bec09a58c506349cd1
Kimmo Kinnunen <kimmok@iki.fi>
2009-12-30 10:21:06 +0200

    add script to demonstrate which webkit repo to checkout

---
+++ scripts/git-clone-webkit.sh
@@ -0,0 +1,5 @@
+#!/bin/bash
+
+# example on how to get the webkit that works with yberbrowser.
+
+git clone git://gitorious.org/~akoivisto/webkit/anttis-webkit.git webkit && cd webkit && git checkout origin/tile-cache -b tile-cache
