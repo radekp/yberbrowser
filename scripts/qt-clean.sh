cde813bd52e837c4ba26117edfd740e760cae8fc
Kimmo Kinnunen <kimmok@iki.fi>
2010-02-15 16:57:49 +0200

    qt-clean.sh: Use $sudo for cleaning and set +x to this file.

--- scripts/qt-clean.sh
+++ scripts/qt-clean.sh
@@ -5,7 +5,7 @@ d=`dirname $0`
 . $d/common.sh
 
 if is_sbox; then
-    (cd $QT_SRC_DIR && ./debian/rules clean)
+    (cd $QT_SRC_DIR && $sudo ./debian/rules clean)
 else
-    (cd $QT_SRC_DIR && make distclean)
+    (cd $QT_SRC_DIR && $sudo make distclean)
 fi
